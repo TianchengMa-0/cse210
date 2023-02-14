541c0830cd92c7e84d68456916488ffdb2d43c40
TianchengMa-0 <59636250+TianchengMa-0@users.noreply.github.com>
2023-02-13 16:29:33 -0800

    Add files via upload

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <test />
+  <br />
+  <div>HelloWorld</div>
+</template>
+
+<script>
+import test from "@/components/test.vue";
+export default {
+  components: {
+    test,
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ frontend/src/components/test.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>qwer</div>
+</template>
+
+<script>
+export default {};
+</script>
